e5f24dea06d435f8c215b860ee9c2dd35f6103bb
Mathis ROME <mathis.rome@icloud.com>
2023-08-28 12:35:17 +0200

    feat : mise en place task and main task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { TaskComponent } from './task/task.component';
+import { MainTaskComponent } from './main-task/main-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    TaskComponent,
+    MainTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/Task.ts
@@ -0,0 +1,14 @@
+export enum TaskStatus {
+  IN_PROGRESS = "in progress",
+  TODO = "todo",
+  DONE = "done",
+}
+
+export class Task {
+  constructor(
+    public title: string,
+    public description: string,
+    public status: string,
+  ) {
+  }
+}
